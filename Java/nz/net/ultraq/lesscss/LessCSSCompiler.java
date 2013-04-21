c96d6e39fcb1db79e1ac90b8d0a944bebf2bf051
Emanuel Rabina <emanuelrabina@gmail.com>
2013-04-21 14:47:03 +1200

    Initial commit.

---
+++ Java/nz/net/ultraq/lesscss/LessCSSCompiler.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2012, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.lesscss;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.tools.shell.Global;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Compiles LESS input into CSS, using less.js on Mozilla Rhino.
+ * 
+ * @author Emanuel Rabina
+ */
+public class LessCSSCompiler {
+
+	private static final String ENV_RHINO_JS = "nz/net/ultraq/lesscss/env.rhino-1.2.13.js";
+	private static final String LESS_JS      = "nz/net/ultraq/lesscss/less-1.3.3.js";
+
+	/* (function() {
+	 * 	 var result = null;
+	 *   var parser = new less.Parser({
+	 *     filename: '{0}'
+	 *   });
+	 * 
+	 *   parser.parse('{1}', function(err, tree) {
+	 *     result = err ? err : tree.toCSS();
+	 *   });
+	 *   return result;
+	 * })();
+	 */
+	private static final String compilejs = "(function() { var result = null; var parser = new less.Parser({ filename: '{0}' }); parser.parse('{1}', function(err, tree) { result = err ? err : tree.toCSS(); }); return result; })();";
+
+	private final Scriptable scope;
+
+	/**
+	 * Create a new LESS compiler.
+	 * 
+	 * @throws LessCSSException If there was a problem initializing the compiler.
+	 */
+	public LessCSSCompiler() throws LessCSSException {
+
+		try {
+			Context context = Context.enter();
+			context.setLanguageVersion(Context.VERSION_1_8);
+			context.setOptimizationLevel(-1);
+			Global global = new Global();
+			global.init(context);
+			scope = context.initStandardObjects(global);
+
+			context.evaluateReader(scope, new BufferedReader(new InputStreamReader(
+					LessCSSCompiler.class.getClassLoader().getResource(ENV_RHINO_JS).openStream())),
+					"env.rhino.js", 1, null);
+			context.evaluateReader(scope, new BufferedReader(new InputStreamReader(
+					LessCSSCompiler.class.getClassLoader().getResource(LESS_JS).openStream())),
+					"less.js", 1, null);
+		}
+		catch (IOException ex) {
+			throw new LessCSSException("Unable to initialize LESS compiler.", ex);
+		}
+		finally {
+			Context.exit();
+		}
+	}
+
+	/**
+	 * Compile the LESS input into CSS.
+	 * 
+	 * @param input	 The LESS file to compile.
+	 * @param output The CSS file to write the compiled result to.
+	 * @throws LessCSSException
+	 */
+	public void compile(File input, File output) throws LessCSSException {
+
+		String result;
+		try (Scanner scanner = new Scanner(input)) {
+			scanner.useDelimiter("\\A");
+			String inputstring = scanner.hasNext() ? scanner.next() : "";
+			result = compile(input.getName(), inputstring);
+		}
+		catch (FileNotFoundException ex) {
+			throw new LessCSSException("Input file " + input.getName() + " doesn't exist", ex);
+		}
+
+		try {
+			if (!output.exists()) {
+				output.createNewFile();
+			}
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+				writer.write(result);
+			}
+		}
+		catch (IOException ex) {
+			throw new LessCSSException("Unable to write compiled CSS", ex);
+		}
+	}
+
+	/**
+	 * Compile the LESS input into CSS.
+	 * 
+	 * @param input The LESS input to compile.
+	 * @return Compiled LESS input.
+	 * @throws LessCSSException
+	 */
+	public String compile(String input) {
+
+		return compile("(inline input)", input);
+	}
+
+	/**
+	 * Compile the LESS input into CSS.
+	 * 
+	 * @param filename Name of the LESS file being compiled.
+	 * @param input	   The LESS input to compile.
+	 * @return Compiled LESS input.
+	 * @throws LessCSSException
+	 */
+	protected String compile(String filename, String input) throws LessCSSException {
+
+		try {
+			Context context = Context.enter();
+			context.setLanguageVersion(Context.VERSION_1_8);
+
+			// Process the LESS input
+			String processless = compilejs.replace("{0}", filename)
+					.replace("{1}", input.replace("'", "\\'").replaceAll("\\s", " "));
+			return context.evaluateString(scope, processless, "process-less.js", 1, null).toString();
+		}
+		catch (JavaScriptException ex) {
+			throw new LessCSSException("Unable to process LESS input from " + filename, ex);
+		}
+		finally {
+			Context.exit();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,58 @@
-lesscss-compiler
-================
 
-A standalone LESS CSS compiler for Java
+LESS CSS Compiler
+=================
+
+A standalone LESS CSS compiler for Java, spun-off from my [LESS CSS Filter](https://github.com/ultraq/lesscss-filter).
+
+ - Current version: 1.0
+ - Released: 21 April 2013
+
+
+Requirements
+------------
+
+ - Java 7
+
+
+Installation
+------------
+
+### Standalone distribution
+Copy the JAR from [the latest download bundle](http://www.ultraq.net.nz/downloads/programming/LessCSS Compiler 1.0.zip),
+or build the project from the source code here on GitHub.
+
+### For Maven and Maven-compatible dependency managers
+Add a dependency to your project with the following co-ordinates:
+
+ - GroupId: `nz.net.ultraq.lesscss`
+ - ArtifactId: `lesscss-compiler`
+ - Version: `1.0`
+
+
+Usage
+-----
+
+[Start writing LESS!](http://lesscss.org/).
+
+In your Java program, create a new `LessCSSCompiler` instance, then call any of
+the `compile()` methods to convert your LESS input into CSS output, eg:
+
+```java
+LessCSSCompiler compiler = new LessCSSCompiler();
+
+// String input/output
+String css = compiler.compile("@some-color: #add65c; #content { border-right: 0.25em solid @some-color; }");
+
+// File input/output
+compiler.compile(new File("input.less"), new File("output.css"));
+```
+
+...and so on.
+
+
+Changelog
+---------
+
+### 1.0
+ - Split processor/compiler part from the filter project.
+ - Update lesscss.js version to 1.3.3.
